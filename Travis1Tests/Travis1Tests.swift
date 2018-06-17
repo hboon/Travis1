a2218b225a49c3fd8fea16d988260c3deec13ad9
Hwee-Boon Yar <hboon@motionobj.com>
2018-06-18 01:18:28 +0800

    First cut

---
+++ Travis1Tests/Travis1Tests.swift
@@ -0,0 +1,18 @@
+//
+//  Travis1Tests.swift
+//  Travis1Tests
+//
+//  Created by Hwee-Boon Yar on Jun/18/18.
+//  Copyright © 2018 MotionObj. All rights reserved.
+//
+
+import XCTest
+@testable import Travis1
+
+class Travis1Tests: XCTestCase {
+    
+    func testFoo() {
+        XCTAssert(1 == 1)
+    }
+    
+}
